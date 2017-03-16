5f564d0a013be524f9108af45321173f61d46cd8
Christopher Steingraber <steingraberchristopher@yahoo.com>
2017-03-16 12:20:06 -0400

    Add and successfully test my_select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -12,4 +12,32 @@ module Enumerable
 		end
 		self
 	end
+
+	def each_with_index
+		if self.instance_of? Array
+			self.length.times do |index|
+				yield(self[index], index)
+			end
+		end
+		self
+	end
+
+	def my_select
+		if self.instance_of? Array
+			result = Array.new
+			self.my_each do |element|
+				if yield(element)
+					result << element
+				end
+			end
+		else
+			result = Hash.new
+			self.my_each do |key, value|
+				if yield(key, value)
+					result[key] = value 
+				end
+			end
+		end
+		result
+	end
 end
